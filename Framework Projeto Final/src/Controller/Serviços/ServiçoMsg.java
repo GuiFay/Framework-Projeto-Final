8f75f101651190856b72001ae3e9c3e54d18f9df
Guilherme Fay Vergara <guifayvergara@hotmail.com>
2013-02-22 13:35:29 -0300

    Commit Inicial do Projeto

---
+++ Framework Projeto Final/src/Controller/Serviços/ServiçoMsg.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Serviços;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class ServiçoMsg implements Serviços{
+
+    @Override
+    public int pesquisarStatus() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void SelecionarUsuário() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String escreverMensagem() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void enviarMensagem() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
